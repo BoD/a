9eaccb9ac6045cab7e9d0b24b20e073f63fa7b9a
BoD <BoD@JRAF.org>
2023-02-11 14:27:13 +0100

    Update libraries, ignore accents in search, action button opens first app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "a.a.a.a"
         minSdk = 26
         targetSdk = 33
-        versionCode = 4
-        versionName = "1.3.0"
+        versionCode = 5
+        versionName = "1.4.0"
     }
 
     signingConfigs {
