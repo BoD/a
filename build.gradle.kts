47cea3946c02bd27eb59f61e24177395457a9c25
BoD <BoD@JRAF.org>
2026-03-31 11:45:26 +0200

    Finish AGP 9 migration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,3 @@
-import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
-
 plugins {
     alias(libs.plugins.androidApplication).apply(false)
-    alias(libs.plugins.benManes.versions)
 }
-
-tasks.withType<DependencyUpdatesTask> {
-    rejectVersionIf {
-        val reject = setOf("alpha", "beta", "rc", "dev")
-        reject.any { candidate.version.contains("-$it", ignoreCase = true) }
-    }
-}
-
-// `./gradlew dependencyUpdates` to see new dependency versions
