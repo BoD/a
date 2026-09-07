44450ae02aa9bcc15c97dc048cc0eea032ff781e
Carmen Alvarez <carmen@rmen.ca>
2026-09-07 11:32:12 +0200

    test(di): Provide a fake app repository for tests.

---
+++ app/src/androidTest/kotlin/org/jraf/android/a/fakes/data/FakeAppRepository.kt
@@ -0,0 +1,42 @@
+/*
+ * This source is part of the
+ *      _____  ___   ____
+ *  __ / / _ \/ _ | / __/___  _______ _
+ * / // / , _/ __ |/ _/_/ _ \/ __/ _ `/
+ * \___/_/|_/_/ |_/_/ (_)___/_/  \_, /
+ *                              /___/
+ * repository.
+ *
+ * Copyright (C) 2026-present Benoit 'BoD' Lubek (BoD@JRAF.org)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.jraf.android.a.fakes.data
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import org.jraf.android.a.data.AppRepository
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class FakeAppRepository @Inject constructor(): AppRepository() {
+    private val _apps = MutableStateFlow<List<App>>(emptyList())
+
+    override val allApps: Flow<List<App>> = _apps
+
+    fun setApps(apps: List<App>) {
+        _apps.value = apps
+    }
+}
